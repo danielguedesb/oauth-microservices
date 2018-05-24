1d76dac13f33d926a3c66e8fb45b2a1e4d2d4695
Eugen Paraschiv <hanriseldon@gmail.com>
2018-05-24 20:28:03 +0300

    introducing zuul proxy

--- api-gateway-zuul/src/main/java/com/qualica/gateway/ApiGatewayApplication.java
+++ api-gateway-zuul/src/main/java/com/qualica/gateway/ApiGatewayApplication.java
@@ -1,9 +1,11 @@
-package com.qualica.demo;
+package com.qualica.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
