9c859d03e2ed8200c50f7598a51217f6eb208b66
Eugen Paraschiv <hanriseldon@gmail.com>
2018-05-25 18:26:39 +0300

    resource server work

--- 2x/oauth-resource-server-1/src/main/java/com/qualica/ResourceServerApplication1.java
+++ 2x/oauth-resource-server-1/src/main/java/com/qualica/ResourceServerApplication1.java
@@ -3,8 +3,10 @@ package com.qualica;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
 public class ResourceServerApplication1 extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
