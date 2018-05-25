4bd51feb096edd6cfd64a525354f8896b19772f5
Eugen Paraschiv <hanriseldon@gmail.com>
2018-05-25 18:15:06 +0300

    general maven cleanup

--- 2x/api-gateway-2-zuul/src/main/java/com/qualica/ApiGatewayApplication.java
+++ 2x/api-gateway-2-zuul/src/main/java/com/qualica/ApiGatewayApplication.java
@@ -8,7 +8,6 @@ import java.util.stream.Stream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoRestTemplateCustomizer;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerInterceptor;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
@@ -21,7 +20,7 @@ import org.springframework.security.oauth2.client.token.grant.password.ResourceO
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableDiscoveryClient
+// @EnableDiscoveryClient
 public class ApiGatewayApplication {
 
     @Bean
